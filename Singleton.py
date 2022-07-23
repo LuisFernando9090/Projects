51f9a77cd817082bdbf8b4b03b5f4b658fc4bdb7
LuisFernando9090 <62106817+LuisFernando9090@users.noreply.github.com>
2022-07-23 09:59:45 -0300

    Add files via upload

---
+++ Singleton.py
@@ -0,0 +1,39 @@
+class MySingleton(type):
+    _instances = {}
+
+    def __call__(mySingletonClass, *args, **kwds):
+        if mySingletonClass not in mySingletonClass._instances:
+            instance = super().__call__(*args, **kwds)
+            mySingletonClass._instances[mySingletonClass] = instance
+        return mySingletonClass._instances[mySingletonClass]
+
+
+class MySingletonTest(metaclass=MySingleton):
+    _userLogin = ""
+    _userPass = ""
+
+    def __init__(self, login, password) -> None:
+        self._userLogin = login
+        self._userPass = password
+
+    def set_userLogin(self, login):
+        self._userLogin = login
+
+    def set_Pass(self, password):
+        self.set_Pass = password
+
+    def get_userLogin(self):
+        return self._userLogin
+
+    def get_userPass(self):
+        return self._userPass
+
+# Instances
+myTest_1 = MySingletonTest("Sif", 123)
+myTest_2 = MySingletonTest("Luis", 469)
+
+# Check if the are equals
+if myTest_1 == myTest_2:
+    print("Error: Login or Password are equals!")
+else:
+    print("Login Successfully!")
